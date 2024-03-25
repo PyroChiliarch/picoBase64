ba384048da26d5bd371c7e1d97a29e5d72f9577c
PyroChiliarch <pyrochiliarch@gmail.com>
2024-03-25 20:05:10 +1000

    updated main.lua after update to v0.1.0c

--- src/main.lua
+++ src/main.lua
@@ -1,23 +1,24 @@
---[[pod_format="raw",created="2024-03-23 13:32:57",modified="2024-03-23 13:34:33",revision=8]]
--- Installs the base64.lua library
+function _init ()
+	-- Installs the base64.lua library
 
-source = "https://github.com/PyroChiliarch/picoBase64/raw/main/base64.lua"
-dest = "/appdata/system/lib/base64.lua"
+	source = "https://github.com/PyroChiliarch/picoBase64/raw/main/base64.lua"
+	dest = "/appdata/system/lib/base64.lua"
 
--- Make the lib folder if it doesn't exist in appdata
-if not (fstat("/appdata/system/lib")) then
-	mkdir ("/appdata/system/lib")
+	-- Make the lib folder if it doesn't exist in appdata
+	if not (fstat("/appdata/system/lib")) then
+		mkdir ("/appdata/system/lib")
+	end
+
+	-- Delete if it already exists
+	-- we want to write over the top
+	if (fstat("/appdata/system/lib/base64.lua")) then
+		rm("/appdata/system/lib/base64.lua")
+	end
+
+	-- Fetch fie form github and write to lib/
+	file = fetch(source)
+	store(dest, file)
+
+	print("base64.lua installed")
+	print("use include("..dest..") in code to get started")
 end
-
--- Delete if it already exists
--- we want to write over the top
-if (fstat("/appdata/system/lib/base64.lua")) then
- rm("/appdata/system/lib/base64.lua")
-end
-
--- Fetch fie form github and write to lib/
-file = fetch(source)
-store(dest, file)
-
-print("base64.lua installed")
-print("use include("..dest..") in code to get started")
